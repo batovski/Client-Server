1691de41c376e1cc41a493e26a59e63eaacf2a87
batovski <56854166+batovski@users.noreply.github.com>
2020-09-27 15:52:16 -0700

    Initial commit

---
+++ Assets/Scripts/ClientHandle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+
+public class ClientHandle : MonoBehaviour
+{
+    public static void Welcome(Packet _packet)
+    {
+        string msg = _packet.ReadString();
+        int _myid = _packet.ReadInt();
+        Debug.Log($"Message from the server: {msg}");
+        Client.instance.myId = _myid;
+        ClientSend.WelcomeReceived();
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+    public static void SpawnPlayer(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        string _username = _packet.ReadString();
+        Vector3 _position = _packet.ReadVector3();
+        Quaternion _rotation = _packet.ReadQuaternion();
+
+        GameManager.instance.SpawnPlayer(_id, _username, _position, _rotation);
+    }
+    public static void PlayerPosition(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Vector3 _postion = _packet.ReadVector3();
+
+        GameManager.players[_id].transform.position = _postion;
+
+    }
+    public static void PlayerRotation(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Quaternion _rotation = _packet.ReadQuaternion();
+
+        GameManager.players[_id].transform.rotation = _rotation;
+
+    }
+}
